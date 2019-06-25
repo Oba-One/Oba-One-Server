4c28c0a4ce0b2bf557d139f794717a08290a3766
01 <oba-one@01s-MacBook-Pro.local>
2019-06-25 03:13:40 -0700

    updated admin api

--- src/api/admin/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	resolvers: require('./admin.resolvers'),
-	typeDefs: require('../../utils/gqlLoader')('admin/admin.graphql'),
-};
--- src/api/admin/resolvers.js
+++ src/api/admin/resolvers.js
@@ -1,4 +1,6 @@
-module.exports = {
+export const admin = {
 	Query: {},
 	Mutation: {},
 };
+
+
