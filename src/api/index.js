54f989d33c9dd025e17d135772291fe13ea0df01
01 <oba-one@01s-MacBook-Pro.local>
2019-06-25 03:14:27 -0700

    updated api index

--- src/api/index.js
+++ src/api/index.js
@@ -1,32 +1,7 @@
-const admin = require('./admin');
-const devices = require('./devices');
-const ecosystem = require('./ecosystem');
-const notification = require('./notification');
-const users = require('./users');
+import { admin } from './admin/resolvers';
+import { users } from './users/resolvers';
+import { devices } from './devices/resolvers';
+import { ecosystem } from './ecosystem/resolvers';
+import { notification } from './notification/resolvers';
 
-const merge = require('lodash/merge');
-
-const db = require('../database');
-
-module.exports = () => ({
-	typeDefs: [
-		admin.typeDefs,
-		devices.typeDefs,
-		ecosystem.typeDefs,
-		notification.typeDefs,
-		users.typeDefs,
-	].join(''),
-	resolvers: merge(
-		{},
-		admin.resolvers,
-		devices.resolvers,
-		ecosystem.resolvers,
-		notification.resolvers,
-		users.resolvers,
-	),
-	context: async () => ({
-		auth: {},
-		db: db(),
-		cache: {},
-	}),
-});
+export { admin, users, devices, ecosystem, notification };
