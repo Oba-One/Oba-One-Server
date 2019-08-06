db5694e287c6a211bc2bafa786d84645fb0b79b3
01 <oba-one@01s-MacBook-Pro.local>
2019-08-06 09:56:51 -0700

    updated ecosystem api

--- src/api/ecosystem/resolvers.js
+++ src/api/ecosystem/resolvers.js
@@ -1,10 +1,20 @@
-const getEcosystem = async (_, args = {}, ctx = {}) => {
+const getEcosystem = async (_, args = { enum: `` }, ctx = {}) => {
 	const ecosystemEnum = args.enum;
 	const ecosystem = await ctx.ecosystem;
 	return ecosystem;
 };
 
-const updateEcosystem = async (_, args = {}, ctx = {}) => {
+const getEcosystems = async (_, args = {}, ctx = {}) => {
+	const ecosystemEnum = args.enum;
+	const ecosystem = await ctx.ecosystem;
+	return ecosystem;
+};
+
+const updateEcosystem = async (
+	_,
+	args = { id: ``, enum: ``, input: {} },
+	ctx = {},
+) => {
 	const id = args.id;
 	const ecosystemEnum = args.enum;
 	const ecosystem = args.input;
@@ -13,6 +23,11 @@ const updateEcosystem = async (_, args = {}, ctx = {}) => {
 };
 
 export const ecosystem = {
-	Query: {},
-	Mutation: {},
+	Query: {
+		getEcosystem,
+		getEcosystems,
+	},
+	Mutation: {
+		updateEcosystem,
+	},
 };
